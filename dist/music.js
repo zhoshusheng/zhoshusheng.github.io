f9188cdaa49c0b5cd48fa77f96156efb0ba41724
zhoshusheng <zhoshusheng@foxmail.com>
2022-04-27 13:17:18 +0800

    Site updated: 2022-04-27 13:17:17

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+	autoplay: true, //�Զ�����
+    audio: [
+	{
+        name: "ԡ��",
+        artist: 'deca joins',
+        url: 'https://m704.music.126.net/20211212133437/6298f876daf4b3a20b984659f71f8968/jdyyaac/000c/560b/5153/7f43625368aa52c4fbb0f968fa2007d2.m4a?authSecret=0000017dad0be60506550aa4681408a0',
+        cover: 'https://p1.music.126.net/byjfkEIOWI_RmxSKEWTPyw==/18910500486297525.jpg?param=200y200',	
+    },
+	]
+});
---
+++ js/diy/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+	autoplay: true, //�Զ�����
+    audio: [
+	{
+        name: '',
+        artist: '',
+        url: '',
+        cover: '',
+    }, 
+	]
+});
